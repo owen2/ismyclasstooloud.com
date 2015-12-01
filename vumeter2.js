12d6361e18ebf72de77778aacf541e556e3e9ba5
Owen Johnson <owen@owenjohnson.info>
2015-11-30 21:46:22 -0600

    fix variable scope
    http://stackoverflow.com/questions/34011538/how-can-i-update-angular-bindings-with-data-from-a-getusermedia-stream/34011749#34011749

--- vumeter2.js
+++ vumeter2.js
@@ -1,34 +1,35 @@
-angular.module('vumeter',[])
-.controller('mainController',function($scope,$interval){
-	this.status= "Please give me access to your mic.";
-	this.loudnessLevel="an unknown loudness";
-	this.history = [];
-	this.peakDebug = 0.0;
-	
-	this.loudnessLevel = "not getting updated.";
-	if (!!!(navigator.mediaDevices.getUserMedia))
-	{
-		this.status = "This browser is not supported.";
-	}
-	else
-	{
-		navigator.mediaDevices.getUserMedia( {audio:true, video:false}).then(function(stream)
-		{
-			this.rawData = new Float32Array(2048);
-			this.audioContext = new AudioContext();
-			this.analyser = this.audioContext.createAnalyser();
-			this.audioContext.createMediaStreamSource(stream).connect(this.analyser); 
-			$interval(function (){
-				this.analyser.getFloatTimeDomainData(this.rawData);
-				var peak = Math.max.apply(Math,this.rawData);
-				$scope.peakDebug = peak;
-				if (peak > .5){
-					$scope.loudnessLevel= "loud!";
-				}
-				else{
-					$scope.loudnessLevel = "not so loud.";
-				}
+angular.module('vumeter', [])
+	.controller('mainController', function ($scope, $interval) {
+		this.deviceStatus = "Loading...";
+		this.loudnessLevel = "an unknown loudness";
+		this.history = [];
+		this.peakDebug = 0.0;
+
+		if (!!!(navigator.mediaDevices.getUserMedia)) {
+			this.deviceStatus = "This browser is not supported.";
+		}
+		else {
+			this.deviceStatus = "Please give me access to your mic.";
+			var controller = this;
+			navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function (stream) {
+				controller.deviceStatus = "";
+				var c = controller;
+				this.rawData = new Float32Array(44100);
+				this.audioContext = new AudioContext();
+				this.analyser = this.audioContext.createAnalyser();
+				this.audioContext.createMediaStreamSource(stream).connect(this.analyser);
+				$interval(function () {
+					this.analyser.getFloatTimeDomainData(this.rawData);
+					var peak = Math.max.apply(Math, this.rawData);
+					c.peakDebug = peak;
+					c.history.push(peak);
+					if (peak > .8) {
+						c.loudnessLevel = "loud!";
+					}
+					else {
+						c.loudnessLevel = "not very loud.";
+					}
+				}, 1000);
 			});
-		}, 1000);
-	}
-});
+		}
+	});
